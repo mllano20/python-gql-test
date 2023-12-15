f07fd650f8de1aacf6480647ee2b92c97971a83e
Marcelo Llano <51483633+mllano20@users.noreply.github.com>
2023-12-14 22:35:58 -0300

    test check

--- models.py
+++ models.py
@@ -7,8 +7,8 @@ import os
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
 
+DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
 db_uri = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
